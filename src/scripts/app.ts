a3cf152c36a91be14eed28593b4c4fe064701b87
SUHEL Makkad <71591136+SuhelMakkad@users.noreply.github.com>
2022-11-20 19:58:03 +0530

    added requestanimationfram call

--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -4,11 +4,15 @@ import { getInitailSeed, getNextGeneration } from "./game";
 const aniamte = (canvas: HTMLCanvasElement, currGeneration: number[][]) => {
   const nextGeneration = getNextGeneration(currGeneration);
   drawMatrix(canvas, currGeneration, 25);
+
+  window.requestAnimationFrame(() => {
+    aniamte(canvas, nextGeneration);
+  });
 };
 
 const start = () => {
   const canvas = document.getElementById("canvas") as HTMLCanvasElement;
-  const seedGeneration = getInitailSeed(20, 20, "glider");
+  const seedGeneration = getInitailSeed(20, 20, "blinker");
 
   steupCanvas(canvas);
   aniamte(canvas, seedGeneration);
